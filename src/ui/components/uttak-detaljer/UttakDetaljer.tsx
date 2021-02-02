3c5239ceeecf7a66006f4541bb36b95a79450038
Hallvard Andreas Stark <hallvard@scel.to>
2021-02-02 09:09:19 +0100

    gradering mot tilsyn

--- src/ui/components/uttak-detaljer/UttakDetaljer.tsx
+++ src/ui/components/uttak-detaljer/UttakDetaljer.tsx
@@ -4,6 +4,9 @@ import { Uttaksperiode } from '../../../types/Uttaksperiode';
 import styles from './uttakDetaljer.less';
 import UttakUtregning from './UttakUtregning';
 import Utbetalingsgrad from '../../../types/Utbetalingsgrad';
+import GraderingMotTilsyn from '../../../types/GraderingMotTilsyn';
+import { Element } from 'nav-frontend-typografi';
+import { beregnDagerTimer } from '../../../util/dateUtils';
 
 const formatUtbetalingsgrader = (utbetalingsgrader: Utbetalingsgrad[]) =>
     utbetalingsgrader.map((utbetalingsgrad) => (
@@ -32,22 +35,56 @@ const getAvslagsetiketter = (uttaksgrad: number) => {
     );
 };
 
+const formatGraderingMotTilsyn = (graderingMotTilsyn: GraderingMotTilsyn) => {
+    const { pleiebehov, etablertTilsyn, andreSøkeresTilsyn, tilgjengeligForSøker } = graderingMotTilsyn;
+    return (
+        <div className={styles.uttakDetaljer__graderingMotTilsyn}>
+            <p className={styles.uttakDetaljer__data}>{`Pleiebehov: ${pleiebehov}%`}</p>
+            <p className={styles.uttakDetaljer__data}>{`- Etablert tilsyn: ${etablertTilsyn}%`}</p>
+            <p className={styles.uttakDetaljer__data}>{`- Andre søkeres tilsyn: ${andreSøkeresTilsyn}%`}</p>
+            <hr />
+            <p>{`= ${tilgjengeligForSøker}% tilgjengelig for søker`}</p>
+        </div>
+    );
+};
+
+const formatAvkortingMotArbeid = (utbetalingsgrader: Utbetalingsgrad[]) => {
+    return (
+        <div className={styles.uttakDetaljer__avkortingMotArbeid}>
+            {utbetalingsgrader.map((utbetalingsgrad, index) => (
+                <div>
+                    <Element className={styles.uttakDetaljer__avkortingMotArbeid__heading}>
+                        {`Arbeidsgiver ${index + 1}`}
+                    </Element>
+                    <p className={styles.uttakDetaljer__data}>
+                        {`Normal arbeidstid: ${beregnDagerTimer(utbetalingsgrad.normalArbeidstid)} timer`}
+                    </p>
+                    <p className={styles.uttakDetaljer__data}>
+                        {`Faktisk arbeidstid: ${beregnDagerTimer(utbetalingsgrad.faktiskArbeidstid)} timer`}
+                    </p>
+                    <p className={styles.uttakDetaljer__avkortingMotArbeid__inntektstap}>Søker inntektstap: 50%</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
 interface UttakDetaljerProps {
     uttak: Uttaksperiode;
 }
 
 const UttakDetaljer = ({ uttak }: UttakDetaljerProps): JSX.Element => {
-    const { utbetalingsgrader, uttaksgrad } = uttak;
+    const { utbetalingsgrader, uttaksgrad, graderingMotTilsyn } = uttak;
 
     return (
         <div className={styles.uttakDetaljer}>
             {getAvslagsetiketter(uttaksgrad)}
             <div className={styles.uttakDetaljer__grid}>
                 <UttakUtregning heading="Gradering mot tilsyn" highlight>
-                    <p>Data her...</p>
+                    {formatGraderingMotTilsyn(graderingMotTilsyn)}
                 </UttakUtregning>
                 <UttakUtregning heading="Avkorting mot arbeid">
-                    <p>Data her...</p>
+                    {formatAvkortingMotArbeid(utbetalingsgrader)}
                 </UttakUtregning>
                 <UttakUtregning heading="Utbetalingsgrad">{formatUtbetalingsgrader(utbetalingsgrader)}</UttakUtregning>
                 <UttakUtregning heading="Oppsummering">
--- src/util/dateUtils.ts
+++ src/util/dateUtils.ts
@@ -1,8 +1,14 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import duration from 'dayjs/plugin/duration';
 
 dayjs.extend(utc);
+dayjs.extend(duration);
 
 export const prettifyDate = (date: string) => {
     return dayjs(date).utc(true).format('DD.MM.YYYY');
+};
+
+export const beregnDagerTimer = (duration: string) => {
+    return dayjs.duration(duration).asHours();
 };
