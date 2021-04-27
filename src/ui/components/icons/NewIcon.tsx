2e8a8cec5c8792fd43b1b725bcfb7bcf09a55062
Hallvard Andreas Stark <hallvard@scel.to>
2021-04-27 15:05:05 +0200

    oppdaterty ny-ikon bump versjon

--- src/ui/components/icons/NewIcon.tsx
+++ src/ui/components/icons/NewIcon.tsx
@@ -3,14 +3,16 @@ import * as React from 'react';
 const NewIcon = () => (
     <div
         style={{
-            width: '1.75rem',
-            height: '1.75rem',
-            background: '#0067c5',
+            width: '1.5rem',
+            height: '1.5rem',
+            background: '#E0F5FB',
             borderRadius: '50%',
-            color: 'white',
-            lineHeight: '1.75rem',
+            color: '#5690A2',
+            lineHeight: '1.5rem',
             textAlign: 'center',
             fontSize: '0.875rem',
+            fontWeight: 'bolder',
+            border: '2px solid #5690A2',
         }}
     >
         Ny
