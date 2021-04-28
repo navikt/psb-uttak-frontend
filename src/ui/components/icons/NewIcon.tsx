5726f1b4d0239f13662fd08f618b5c18d1ae384b
Hallvard Andreas Stark <hallvard@scel.to>
2021-04-28 14:21:51 +0200

    forbedret ny-ikonet bump versjon

--- src/ui/components/icons/NewIcon.tsx
+++ src/ui/components/icons/NewIcon.tsx
@@ -3,12 +3,12 @@ import * as React from 'react';
 const NewIcon = () => (
     <div
         style={{
-            width: '1.5rem',
-            height: '1.5rem',
+            width: '1.75rem',
+            height: '1.75rem',
             background: '#E0F5FB',
             borderRadius: '50%',
             color: '#5690A2',
-            lineHeight: '1.5rem',
+            lineHeight: '1.625rem',
             textAlign: 'center',
             fontSize: '0.875rem',
             fontWeight: 'bolder',
