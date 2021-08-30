ff729a3674994f3fea0910decca05c28103d9ff9
vebnor <vebjorn.nordby@hotmail.com>
2021-08-30 10:32:47 +0200

    oppdaterer webpack ++

--- webpack/webpack-config.development.js
+++ webpack/webpack-config.development.js
@@ -22,25 +22,28 @@ const webpackConfig = merge(commonWebpackConfig, {
                 mode: 'write-references',
             },
         }),
+        new webpack.HotModuleReplacementPlugin()
     ],
 });
 
+const port = 8081;
 const devServerOptions = {
     hot: true,
     headers: {
         'Access-Control-Allow-Origin': 'http://localhost:9000',
     },
+    port
 };
-WebpackDevServer.addDevServerEntrypoints(webpackConfig, devServerOptions);
 
 const compiler = webpack(webpackConfig);
-const devServer = new WebpackDevServer(compiler, devServerOptions);
+const devServer = new WebpackDevServer(devServerOptions, compiler);
 compiler.close(() => console.info('Compiler closed'));
 
-const port = 8081;
-devServer.listen(port, 'localhost', (error) => {
+devServer.startCallback((error) => {
     if (error) {
-        return console.error(error);
+        console.error(error);
     }
-    console.log(`Listening at port ${port}`);
+    else {
+        console.log(`Listening at port ${port}`);
+    }
 });
