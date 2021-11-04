7ab23d9954aee08cc3f314ebb4af0291dfbcac16
vebnor <vebjorn.nordby@nav.no>
2021-11-04 07:15:32 +0100

    fjerner opptjeningsvilkåret igjen

--- src/vilkårsliste/Vilkår.ts
+++ src/vilkårsliste/Vilkår.ts
@@ -8,10 +8,6 @@ const vilkår = [
         kode: 'FP_VK_3',
     },
     {
-        name: 'Opptjening',
-        kode: 'FP_VK_23', // Opptjeningsvilkåret
-    },
-    {
         name: 'Omsorgen for',
         kode: 'K9_VK_1',
     },
